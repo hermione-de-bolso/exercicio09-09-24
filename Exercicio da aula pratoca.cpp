9b12664e99a9131c60bcec8f0e578353356a7aa3
hermione-de-bolso <nicolegs.ngs@gmail.com>
2024-09-09 11:29:11 -0300

    Create Exercicio da aula pratoca.cpp

---
+++ Exercicio da aula pratoca.cpp
@@ -0,0 +1,24 @@
+// Exercicio para resolver uma equação polinomial
+
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    // Declaração de variaveis
+    double a1, a2, a3;
+    
+    // O que fornecer ao usuario
+    cout << ("\nEntre com o valor do coeficiente quadratico: ");
+    cin >> a1;
+    
+    cout << ("\nEntre com o valor so coeficiente linear: ");
+    cin >> a2;
+    
+    cout << ("\nEntre com op valor do termo independente: ");
+    cin >> a3;
+    
+    
+}
